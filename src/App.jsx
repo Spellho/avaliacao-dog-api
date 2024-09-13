375bb0d91e1f6709673b2d7b992b87d1f8dd2305
Spellho <i_amaral@ymail.com>
2024-09-13 20:31:55 -0300

    Barebones

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Random from './pages/Random'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
